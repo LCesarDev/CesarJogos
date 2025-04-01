a6bc2a60242f3d6ddbaa35b41438fdae03258539
LucasCesar <lucas.c.ndeoliveira@outlook.com>
2025-04-01 16:42:56 -0300

    Buttons/2commit

--- src/HeaderHome.jsx
+++ src/HeaderHome.jsx
@@ -7,19 +7,19 @@ function HeaderHome() {
         <img className="logo" src="/Ativo 5.png" alt="Logo da empresa" />
         
         <nav className="nav-menu" aria-label="Menu principal">
-            <button className="home-button">Home</button>
+           
             
-            <ul className="nav-links">
+        <button className="home-button">Home</button><ul className="nav-links">
                 <li><button className="nav-button">Jogos</button></li>
                 <li><button className="nav-button">Loja</button></li>
                 <li><button className="nav-button">Sobre</button></li>
                 <li><button className="nav-button">Contato</button></li>
             </ul>
 
-            <div className="auth-buttons">
-                <button className="register-button">Cadastrar</button>
-                <button className="login-button">Entrar</button>
-            </div>
+            <ul className="auth-buttons">
+                <li><button className="auth-button">Cadastrar</button></li>
+                <li><button className="auth-button">Entrar</button></li>
+            </ul>
         </nav>
     </header>
     );
