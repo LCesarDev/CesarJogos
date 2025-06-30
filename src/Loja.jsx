ef32eb39af7b390767461d6f8530690e41c7f70f
LucasCesar <lucas.c.ndeoliveira@outlook.com>
2025-06-30 17:04:24 -0300

    feat: criação da página Loja com exibição dinâmica de produtos (mock JSON)

--- src/Loja.jsx
+++ src/Loja.jsx
@@ -1,14 +1,29 @@
 import React from "react";
 import "./Loja.css";
+import items from "./StoreItens.js";
 
 const Loja = () =>{
     return (
+        
+        <div className="Loja-Content">
+            <h1 className="Loja-Title">Loja - Em processo - Consumo de API</h1>
+            {items.map(item =>(
+                <div key={item.id} className="store-style">
+                    <div className="store-infobox">
+                        <div className="store-name">{item.name}
+                            <p>R$ {item.price}</p>
+                            <button className="store-pay"> Pay</button>
+                        </div>
+                    </div>
+                    
+                    <img className="store-image" src={item.image} alt={item.name}></img>
+                </div>
+            ))}
 
-        <div className="LojaContent">
-            <h1 className="LojaTitulo">Loja</h1>
-            <p className="Loja-txt">Bem-bem vindo a Loja</p>
+       
+            
         </div>
     );
-};
+}
 
 export default Loja;
---
+++ src/StoreItens.js
@@ -0,0 +1,18 @@
+
+const store = [
+    {
+        "id": 1,
+        "name": "DangoKart",
+        "price": "15.99",
+        "image": "/Logo_DangoKart.png"
+    },
+
+    {
+        "id": 2,
+        "name": "Desespero",
+        "price": "10.15",
+        "image": "/Desespero.png"
+    },
+]
+
+export default store;
