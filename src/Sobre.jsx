f3ecae12cdc7ded3acbaad768f6bc6f8a4ff8ea5
LucasCesar <lucas.c.ndeoliveira@outlook.com>
2025-07-02 12:49:15 -0300

    Alteracao line-height fonte, melhorar qualidade da letra para leitura

--- src/Sobre.jsx
+++ src/Sobre.jsx
@@ -7,7 +7,7 @@ const Sobre = () =>{
         <div className="content">
             <h1 className="titulo">Sobre</h1>
             <div className="caixatxt">
-            <p className="txt">Olá! Me chamo Lucas César, sou formado em Jogos Digitais pela Universidade FUMEC e atualmente me especializo como Desenvolvedor Web Front-End. A tecnologia sempre fez parte da minha vida, e minha paixão por criar experiências interativas começou nos jogos — hoje, essa paixão se estende ao desenvolvimento de sites e interfaces que unem criatividade, funcionalidade e boa performance.
+            <p className="txt">Olá! Me chamo Lucas César, sou formado em Jogos Digitais pela Universidade FUMEC e atualmente me especializo como Desenvolvedor Web Front-End. A tecnologia sempre fez parte da minha vida, e minha paixão por criar experiências interativas começou nos jogos hoje, essa paixão se estende ao desenvolvimento de sites e interfaces que unem criatividade, funcionalidade e boa performance.
 
 Minha trajetória como desenvolvedor de jogos me proporcionou uma base sólida em lógica de programação, design responsivo, experiência do usuário (UX), e trabalho com interfaces visuais dinâmicas. Trabalhei com Unity, Unreal Engine e Photon para criar jogos tanto singleplayer quanto multiplayer, passando por todas as etapas do desenvolvimento: da programação ao design, da estruturação ao teste final. Essa experiência me ensinou a importância do detalhe, da performance e da clareza visual, qualidades que hoje aplico diretamente no desenvolvimento web.
 
@@ -15,7 +15,7 @@ Recentemente, decidi migrar para o universo do desenvolvimento web, buscando nov
 
 Este site da César Jogos é mais do que um portfólio: é uma ponte entre as duas áreas que mais me desafiaram e moldaram como desenvolvedor, os jogos e o web front-end. Aqui, compartilho minhas criações, minha evolução e meu olhar único sobre desenvolvimento. Acredito que a combinação de pensamento lógico, criatividade e atenção ao detalhe é o que transforma uma ideia em uma experiência memorável.
 
-Sou um profissional em constante aprendizado, com sede por aprimoramento e sempre pronto para colaborar em projetos que envolvam tecnologia, interatividade e inovação. Seja desenvolvendo um site responsivo, um sistema interativo ou um jogo cheio de mecânicas criativas, meu compromisso é sempre entregar algo de valor — tanto para o usuário quanto para o cliente.
+Sou um profissional em constante aprendizado, com sede por aprimoramento e sempre pronto para colaborar em projetos que envolvam tecnologia, interatividade e inovação. Seja desenvolvendo um site responsivo, um sistema interativo ou um jogo cheio de mecânicas criativas, meu compromisso é sempre entregar algo de valor, tanto para o usuário quanto para o cliente.
 
 Se quiser conhecer melhor meu trabalho ou bater um papo sobre desenvolvimento, estou sempre aberto a novas conexões. Obrigado por visitar meu site e fazer parte dessa jornada!🚀</p>
             </div>
