1a7d4e6e68f603428997af7d0398ae62e322572d
LCesarDev <lucas.c.ndeoliveira@outlook.com>
2025-07-09 22:21:59 -0300

    animation Dango pag Home, Alteracao width botao cadastrar, alteracao width img loja p/ mobile

--- src/Home.jsx
+++ src/Home.jsx
@@ -9,9 +9,14 @@ function Home() {
     return(
        <main className="HomeContent">
 
+        
+
+
         <h1 className="TitleHome_Animation">Bem-vindo a César Jogos!</h1>
         <p className="TextHome_Animation">Aqui, a diversão e a criatividade estão sempre em alta. Explore nossos jogos e divirta-se!</p>
         <nav className=""><button className="Jogar-HomeButton" onClick={()=> navigate("/Jogos")}>Quero Jogar!</button></nav>
+
+        <img src="./AnimationDango.gif" alt="DangoGIf" className="dangogif"></img>
 
        </main>
     );
